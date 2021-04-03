634387af4c14f1b84a47ae20b0c5e684e4834125
thetek42 <git@thetek.de>
2021-04-03 13:19:54 +0200

    🍱 add favicon ✏️ fix typo scRipt

--- ui/compile.sh
+++ ui/compile.sh
@@ -7,13 +7,17 @@ pug raw/index.pug -o html
 echo "<title>dnswatch</title>
 <link rel=\"stylesheet\" href=\"index.css\">
 <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;700&amp;display=swap\">
+<link rel=\"shortcut icon\" type=\"image/png\" href=\"favicon.png\">
 <script src=\"index.js\"></script>
 <script src=\"https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js\"></script>" | cat - html/index.html > temp
 mv temp html/index.html
-echo "<scipt src=\"feather.js\"></script>" >> html/index.html
+echo "<script src=\"feather.js\"></script>" >> html/index.html
 
 # compile sass
 sass raw/index.sass html/index.css
 
 # move js
 cp raw/*.js html
+
+# move favicon
+cp raw/favicon.png html
