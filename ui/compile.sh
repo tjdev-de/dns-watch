ae4104178eee37fa286d77b874824bb8c87fbb44
thetek42 <git@thetek.de>
2021-04-03 14:03:12 +0200

    🩹 utf8 warning

--- ui/compile.sh
+++ ui/compile.sh
@@ -8,6 +8,8 @@ echo "<title>dnswatch</title>
 <link rel=\"stylesheet\" href=\"index.css\">
 <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;700&amp;display=swap\">
 <link rel=\"shortcut icon\" type=\"image/png\" href=\"favicon.png\">
+<meta content=\"text/html;charset=utf-8\" http-equiv=\"Content-Type\">
+<meta content=\"utf-8\" http-equiv=\"encoding\">
 <script src=\"index.js\"></script>
 <script src=\"https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js\"></script>" | cat - html/index.html > temp
 mv temp html/index.html
