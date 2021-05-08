6bf64a3b7cbe13039ddabea8d5f95d5a83270517
DrMaxNix <dr.maxnix@gmail.com>
2021-05-08 19:43:23 +0200

    :sparkles: add backend/api code

---
+++ config/lookup.php
@@ -0,0 +1,15 @@
+<?php
+	$§DNSWATCH_LOOKUP_TMP_DIR = "/tmp";
+	
+	
+	// REFERENCE //
+	//how many references to use
+	$§DNSWATCH_LOOKUP_REFERENCE_COUNT = 3;
+	
+	//how many references have to find something to continue
+	$§DNSWATCH_LOOKUP_REFERENCE_USEABLE_MIN = 2;
+	
+	
+	// CAUSE-GUESSING //
+	$§DNSWATCH_LOOKUP_CAUSE_GUESS["cuii"] = [["type" => "cname", "cname" => "notice.cuii.info."]];
+?>
