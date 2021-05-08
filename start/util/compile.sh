63147f64637f2b66340247b522cea1f20aec8a3d
thetek42 <git@thetek.de>
2021-05-08 19:23:38 +0200

    ♻ refactor ui → start

--- start/util/compile.sh
+++ start/util/compile.sh
@@ -3,7 +3,7 @@
 ### compile to regular html and css
 
 # compile pug
-pug raw/index.pug -o html
+pug ../src/index.pug -o ../html
 echo "<title>dnswatch</title>
 <link rel=\"stylesheet\" href=\"index.css\">
 <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;700&amp;display=swap\">
@@ -11,15 +11,15 @@ echo "<title>dnswatch</title>
 <meta content=\"text/html;charset=utf-8\" http-equiv=\"Content-Type\">
 <meta content=\"utf-8\" http-equiv=\"encoding\">
 <script src=\"index.js\"></script>
-<script src=\"https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js\"></script>" | cat - html/index.html > temp
-mv temp html/index.html
-echo "<script src=\"feather.js\"></script>" >> html/index.html
+<script src=\"https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js\"></script>" | cat - ../html/index.html > temp
+mv temp ../html/index.html
+echo "<script src=\"feather.js\"></script>" >> ../html/index.html
 
 # compile sass
-sass raw/index.sass html/index.css
+sass ../src/index.sass ../html/index.css
 
 # move js
-cp raw/*.js html
+cp ../src/*.js ../html
 
 # move favicon
-cp raw/favicon.png html
+cp ../src/favicon.png ../html
