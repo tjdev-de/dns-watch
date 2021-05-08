5bffecf09ec659d9ef0594edfe004c551e137b8c
thetek <git@thetek.de>
2021-05-08 12:08:17 +0200

    ✨ help button pop up (#11)
    * ✨ help button popups
    
    * 🎨 update left out table generation code
    
    * 📦️ compile jhtml

--- ui/jhtml-compat/index.js
+++ ui/jhtml-compat/index.js
@@ -28,8 +28,6 @@ function dnswatch_search() {
 		if (request.status >= 200 && request.status < 400) {
 			var response = JSON.parse(this.response);
 
-			// TODO: CLEAN UP THIS MESS OF TABLE GENERATION CODE
-
 			if (response['type'] == 'success') {   // success
 
 				var table = '<table>\n';
@@ -46,7 +44,8 @@ function dnswatch_search() {
 						dns_address:  i['address'],
 						desc_color:   'ref',
 						desc_text:    'reference',
-						show_help:    first_ref
+						show_help:    first_ref,
+						help_type:    'reference'
 					})
 					first_ref = false;
 
@@ -56,7 +55,7 @@ function dnswatch_search() {
 
 				// randomized response message
 				if (response['data']['randomized_response']) {
-					table += '<small class="random">This domain uses randomized responses. <button>Learn more</button></small>\n';
+					table += '<small class="random">This domain uses randomized responses. <button onclick="show_modal(\'random\')">Learn more</button></small>\n';
 				}
 
 				if (response['data']['found']) {   // only if reference confirms
@@ -73,7 +72,8 @@ function dnswatch_search() {
 							dns_address:  i['address'],
 							desc_color:   i['cause'] !== null ? 'blocked': '',
 							desc_text:    get_blocked_text(i['cause']),
-							show_help:    i['cause'] !== null && !causes.includes(i['cause'])
+							show_help:    i['cause'] !== null && !causes.includes(i['cause']),
+							help_type:    'blocked-' + i['cause']
 						})
 
 						if (i['cause'] !== null) causes.push(i['cause'])
@@ -92,20 +92,19 @@ function dnswatch_search() {
 					document.getElementById('invaliddomain').style.display = 'block';
 				} else {
 					var table = '<table>\n';
-					table += '<tr>\n';
-					// status
-					table += '<td class="status cross">\n';
-					table += '<i data-feather="x"></i>\n';
-					table += '</td>\n';
-					// provider icon
-					table += '<td class="icon">\n';
-					table += '<i data-feather="x"></i>\n';
-					table += '</td>\n';
-					// name and address
-					table += `<td class="name">ERROR: ${ response["error"] }</td>\n`;
-					table += '<td class="desc ref"></td>\n';
-					table += '<td class="help"></td>\n';
-					table += '</tr>\n';
+
+					table += gen_tablerow({
+						status_color: 'cross',
+						status_icon:  'x',
+						dns_icon:     'x',
+						dns_name:     `ERROR: ${ response['error'] }`,
+						dns_address:  '',
+						desc_color:   'ref',
+						desc_text:    '',
+						show_help:    false,
+						help_type:    ''
+					})
+
 					table += '</table>';
 				}
 
@@ -139,7 +138,7 @@ function gen_tablerow(options) {
 	<td class="icon"><img class="uncolored-svg" src="${ options.dns_icon }"></td>
 	<td class="name">${ options.dns_name }<small>${ options.dns_address }</small></td>
 	<td class="desc ${ options.desc_color }">${ options.desc_text }</td>
-	${ options.show_help ? '<td class="help"><button><i data-feather="help-circle"></i></button></td>' : '' }
+	${ options.show_help ? '<td class="help"><button onclick="show_modal(\''+options.help_type+'\')"><i data-feather="help-circle"></i></button></td>' : '' }
 </tr>
 `
 }
@@ -164,6 +163,36 @@ function get_blocked_text(cause) {
 
 
 
+function show_modal(type) {
+	let msg;
+	switch (type) {
+		case 'search':
+			msg = '<b>dns-watch.org</b> is a service that allows you to see how (mainly German) Internet Service Providers (ISPs) deal with Internet censorship. <a href="/why">More Information</a><br>In order to test a website for availability, enter the domain into the search bar and press Enter.<br><span>Try out these examples if you are unsure: gnu.org, wikipedia.org, s.to</span>';
+			break;
+		case 'reference':
+			msg = 'In order to validate the DNS responses, we use <b>references</b>.<br>These references are from trustworthy DNS providers, such as Cloudflare or UncensoredDNS.<br>The responses of the DNS providers that should be tested (located in the bottom table) will be compared to the results from the references.'
+			break;
+		case 'blocked-cuii':
+			msg = 'This ISP (and all others with the same error message) blocked the request on behalf of <b>CUII</b>.<br>CUII is a German institution that censors parts of the Internet because of so-called copyright claims.<br>For more information about the CUII, read our <a href="/why">Why?</a> page.'
+			break;
+		case 'random':
+			msg = 'This domain uses <b>randomized responses</b>.<br>Some websites return different IP addresses for load balancing.<br>Therefore, dns-watch.org can\'t compare the results of these DNS providers and will only check for common blocking patterns.'
+			break;
+		default:
+			msg = `It seems like something happend that shouldn\'t have happened!<br><span>Dialog message type: ${ type }</span>`
+			break;
+	}
+
+	document.getElementById('modal-message').innerHTML = msg
+	document.getElementById('modal').style.display = 'block'
+}
+
+function close_modal() {
+	document.getElementById('modal').style.display = 'none'
+}
+
+
+
 // add event triggers
 document.addEventListener('DOMContentLoaded', () => {
 	document.getElementById('dnswatch-search-submit').addEventListener('click', (event) => {
@@ -176,4 +205,9 @@ document.addEventListener('DOMContentLoaded', () => {
 			dnswatch_search();
 		}
 	});
+
+	window.onclick = function (event) {
+		if (event.target == document.getElementById('modal'))
+			close_modal()
+	}
 });
